c52f0056ad59cee5f237bb0eef4cb004cded4bae
Lu Chaohai <chlu@best-inc.com>
2024-11-06 23:14:44 +0800

    简化index.js文件内容； 用户仅需配置JSON格式的信息即可。

--- index.js
+++ index.js
@@ -1,77 +1,28 @@
+const Main = require('./src/main.js');
+
 // 配置信息 start
 const CONFIG = {
   "文件名称": "PEPXiaoXue5_2.json",
+  "列信息": [
+    {
+      "标题": "单词",
+      "数据标识": "wordHead",
+      "宽度": 32,
+    },
+    {
+      "标题": "音标",
+      "数据标识": "usphone",
+      "宽度": 20,
+    },
+    {
+      "标题": "翻译",
+      "数据标识": "trans",
+      "宽度": 20
+    }
+  ]
 }
 // 配置信息 end
 
-const dictName = CONFIG["文件名称"];
-const fs = require('fs');
-const path = require('path');
-const ExcelJS = require('exceljs');
-
-// 指定要读取的文件路径
-const filePath = path.join(__dirname, dictName); // 假设文件在当前目录下，名为 example.txt
-
-function transToString(trans) {
-  return trans.map(item => {
-    const pos = item.pos ? `[${item.pos}].` : '';
-    return `${pos}${item.tranCn}`
-  }).join('; ')
-}
-
-function handleWord(line) {
-  if (!line) return '';
-  const result = JSON.parse(line);
-  const {
-    wordHead,
-    content
-  } = result.content.word;
-  const {
-    usphone,
-    trans
-  } = content;
-  return {
-    wordHead,
-    usphone,
-    trans: transToString(trans)
-  };
-}
-
-// 使用 fs.readFile 方法读取文件
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    // 如果读取文件时发生错误，打印错误信息
-    console.error('Error reading file:', err);
-    return;
-  }
-  // 如果读取成功，打印文件内容
-  const lines = data.split('\n').filter(line => line).map(line => handleWord(line));
-  generateExcel(lines);
-});
-
-function generateExcel(records) {
-  const workbook = new ExcelJS.Workbook();
-  workbook.creator = 'Me';
-  workbook.lastModifiedBy = 'Her';
-  workbook.created = new Date();
-  workbook.modified = new Date();
-  workbook.lastPrinted = new Date();
-
-  const sheet = workbook.addWorksheet('My Sheet');
-
-  sheet.columns = [{
-    header: '单词',
-    key: 'wordHead',
-    width: 32
-  }, {
-    header: '音标',
-    key: 'usphone',
-    width: 32
-  }, {
-    header: '翻译',
-    key: 'trans',
-    width: 32
-  }];
-  records.forEach(record => sheet.addRow(record));
-  workbook.xlsx.writeFile('词汇表.xlsx');
-}
+const app = new Main();
+app.loadConfig(CONFIG);
+app.run();
